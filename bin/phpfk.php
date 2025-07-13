83ead9605a50fa05ca24d002d1e53c5787e2e730
Rouzan407 <rouzan407@gmail.com>
2025-07-13 07:04:29 +0700

    Add files via upload

---
+++ bin/phpfk.php
@@ -0,0 +1,32 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+if (count($argv) < 3) {
+    fwrite(STDERR, "Usage: php {$argv[0]} <input_php_file> <output_output_file>\n");
+    exit(1);
+}
+
+$inputFile = $argv[1];
+$outputFile = $argv[2];
+
+if (!file_exists($inputFile)) {
+    fwrite(STDERR, "Input file '$inputFile' not found.\n");
+    exit(2);
+}
+
+$inputCode = file_get_contents($inputFile);
+if ($inputCode === false) {
+    fwrite(STDERR, "Cannot read input file '$inputFile'.\n");
+    exit(3);
+}
+
+$inputCode = preg_replace('/^\s*<\?php\s*/', '', $inputCode, 1);
+
+$inputCode = preg_replace('/\?>\s*$/', '', $inputCode);
+
+$obfuscatedCode = \bviguier\PhpFk\obfuscateCode($inputCode);
+
+file_put_contents($outputFile, "<?php set_time_limit(0);error_reporting(0);ini_set('memory_limit','-1');" . $obfuscatedCode . "?>");
+
+echo "Obfuscation done. Output saved to $outputFile.\n";
